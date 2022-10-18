3e203d5e795cad8bea26249eb1391b3484976aa1
Trung Tran <trannguyenbaotrung@gmail.com>
2022-10-19 06:07:27 +0700

    Add Topic Card composable

--- app/src/main/java/com/example/mycourses/MainActivity.kt
+++ app/src/main/java/com/example/mycourses/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.mycourses
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mycourses.model.Topic
 import com.example.mycourses.ui.theme.MyCoursesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
@@ -30,14 +34,60 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card(elevation = 4.dp) {
+        Row() {
+            Box {
+                Image(
+                    painter = painterResource(id = topic.imageId),
+                    contentDescription = null,
+                    modifier = modifier
+                        .size(width = 68.dp, height = 68.dp)
+                        .aspectRatio(1f),
+                    contentScale = ContentScale.Crop
+                )
+            }
+
+            Column() {
+                Text(
+                    text = stringResource(id = topic.nameStringId), modifier = Modifier.padding(
+                        start = 16.dp,
+                        top = 16.dp,
+                        end = 16.dp,
+                        bottom = 8.dp
+                    ), style = MaterialTheme.typography.body2
+                )
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(
+                                start = 16.dp,
+                                end = 8.dp
+                            )
+                            .size(12.dp)
+                    )
+                    Text(
+                        text = topic.students.toString(), style = MaterialTheme.typography.caption
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    MyCoursesTheme {
-        Greeting("Android")
+fun TopicPreview() {
+    MyCoursesTheme() {
+        val topic = Topic(R.string.photography, 321, R.drawable.photography)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TopicCard(topic = topic)
+        }
     }
 }
